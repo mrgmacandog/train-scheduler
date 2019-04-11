f5c0e3d5c3080a92c07c3c9eef44da553f53c4be
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-04-10 22:32:53 -0700

    Modify next train caluculation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
+// Code is executed in strict mode
+"use strict";
+
 // Wrap all code in an anonymous function so functions cannot be called externally
 (function () {
-    // Code is executed in strict mode
-    "use strict";
 
     $(document).ready(function () {
         // Initialize Firebase
@@ -18,62 +19,87 @@
         // Assign the the firebase databse as a variable
         let database = firebase.database();
 
+        // Add options to a dropdown
+        function populateDropdowns(dropdown, min, max) {
+            for (let i = min; i <= max; i++) {
+                if (i < 10) {
+                    dropdown.append($("<option>").text(`0${ i }`));
+                } else {
+                    dropdown.append($("<option>").text(i));
+                }
+            }
+        }
+
+        // Add options to the military hour dropdown
+        populateDropdowns($("#military-hour"), 0, 23);
+        // Add options to the military minute dropdown
+        populateDropdowns($("#military-minute"), 0, 59);
+
+        function validateInput() {
+            return true;
+        }
+
         // When the submit button is clicked
         $("#submit").on("click", function() {
             // Prevent the page from reloading
             event.preventDefault();
+
+            console.log($("#military-hour").val());
             
             // Grabs user input
             let trainName = $("#train-name").val().trim();
             let destination = $("#destination").val().trim();
-            let firstTime = $("#first-time").val().trim();
+            let firstTimeHour = $("#military-hour").val();
+            let firstTimeMinute = $("#military-minute").val();
             let frequency = $("#frequency").val().trim();
 
-            // Creates local "temporary" object for holding train data
-            let newTrain = {
-                trainName: trainName,
-                destination: destination,
-                firstTime: firstTime,
-                frequency: frequency
-            };
+            // Add to database if all input fields are valid
+            if (validateInput()) {
 
-            // Uploads train data to the database
-            database.ref().push(newTrain);
+                // Creates local "temporary" object for holding train data
+                let newTrain = {
+                    trainName: trainName,
+                    destination: destination,
+                    firstTime: `${ firstTimeHour }:${ firstTimeMinute }`,
+                    frequency: frequency
+                };
 
-            // Clears all of the text-boxes
-            $("#train-name").val("");
-            $("#destination").val("");
-            $("#first-time").val("");
-            $("#frequency").val("");
+                // Uploads train data to the database
+                database.ref().push(newTrain);
+
+                // Clears the form
+                $("#train-name").val("");
+                $("#destination").val("");
+                $("#first-time").val("");
+                $("#military-hour").val(0);
+                $("#military-minute").val(0);
+                $("#frequency").val("");
+            }
         });
 
         // When a child is added in the database
         database.ref().on("child_added", function (childSnapshot) {
-            console.log(childSnapshot.val());
-        
             // Store everything into a variable.
             let trainName = childSnapshot.val().trainName;
             let destination = childSnapshot.val().destination;
             let firstTime = childSnapshot.val().firstTime;
             let frequency = childSnapshot.val().frequency;
         
-            // Train Info
-            console.log(trainName);
-            console.log(destination);
-            console.log(firstTime);
-            console.log(frequency);
-        
             // Calculate minutes away
-            // TODO: Implement what happens if it's currently before the first time
             let nowMomment = moment();
             let firstTimeMoment = moment(firstTime, "HH:mm");
             let diffMinutes = nowMomment.diff(firstTimeMoment, "minutes");
-            console.log("Diff Minutes:", diffMinutes);
-            let minAway = frequency - (diffMinutes % frequency)
-            console.log("Minutes Away:", minAway);
+            let minAway;
+            // Case for when the current time is past the first train time
+            if (diffMinutes >= 0) {
+                minAway = frequency - (diffMinutes % frequency);
+            // Case for when the current time is before the first train time
+            } else {  // (difMinutes < 0)
+                minAway = Math.abs(diffMinutes) + 1;
+            }
 
             // Calculate next arrival time
-            let nextArrival = nowMomment.add(minAway, "minutes").format("HH:mm A");
+            let nextArrival = nowMomment.add(minAway, "minutes").format("hh:mm A");
         
             // Create the new row
             let newRow = $("<tr>").append(
